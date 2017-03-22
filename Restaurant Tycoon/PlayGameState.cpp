2a82bbb4c531fa351dc89064f22d71af37f2d537
LAPTOP-G9792KV2\Krzysztof Reniecki <polkom21@gmail.com>
2017-03-22 22:48:16 +0100

    Test działania Gui i elementu Label.

--- Restaurant Tycoon/PlayGameState.cpp
+++ Restaurant Tycoon/PlayGameState.cpp
@@ -5,10 +5,13 @@ void PlayGameState::Draw(const float dt)
 	this->game->window.draw(test);
 
 	this->map->Draw(this->game->window);
+
+	this->gui->Draw(this->game->window);
 }
 
 void PlayGameState::Update(const float dt)
 {
+	this->gui->Update(dt);
 }
 
 void PlayGameState::HandleInput()
@@ -38,6 +41,7 @@ PlayGameState::PlayGameState(Game * game)
 {
 	this->game = game;
 
+	this->game->assetsManager.LoadFont("arial", "Data/arial.ttf");
 	this->game->assetsManager.LoadTextureAtlas("Data/all.atlas");
 	this->game->assetsManager.CreateObject("floor", ObjectType::FLOOR, "all.floor");
 	
@@ -47,6 +51,13 @@ PlayGameState::PlayGameState(Game * game)
 	this->test.setPosition(200, 200);
 
 	this->map = new Map(sf::Vector2i(5, 5), (sf::Vector2f)this->game->window.getSize(), this->game->assetsManager);
+
+	this->gui = new Gui(this->game->assetsManager);
+
+	GuiLabel * label = (GuiLabel*)this->gui->CreateElement("testLabel", GuiElementType::LABEL);
+	label->SetText("Label");
+	label->SetFont(this->game->assetsManager.GetFont("arial"));
+	label->SetTextColor(sf::Color::Green);
 }
 
 
--- Restaurant Tycoon/PlayGameState.h
+++ Restaurant Tycoon/PlayGameState.h
@@ -2,6 +2,8 @@
 #include <SFML\Graphics.hpp>
 #include "GameState.h"
 #include "Map.h"
+#include "Gui.h"
+#include "GuiLabel.h"
 
 class PlayGameState :
 	public GameState
@@ -10,6 +12,7 @@ private:
 	Game * game;
 	sf::RectangleShape test;
 	Map * map;
+	Gui * gui;
 public:
 	virtual void Draw(const float dt);
 	virtual void Update(const float dt);
